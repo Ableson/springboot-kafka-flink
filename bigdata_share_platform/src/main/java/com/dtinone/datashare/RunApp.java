e9f5e051317122e80c0c1276f5bb0554019dfbaf
PJF <1501105594@qq.com>
2020-08-24 17:21:20 +0800

    资源监控数据获得

--- bigdata_share_platform/src/main/java/com/dtinone/datashare/RunApp.java
+++ bigdata_share_platform/src/main/java/com/dtinone/datashare/RunApp.java
@@ -1,20 +1,24 @@
 package com.dtinone.datashare;
 
+import com.cdjiamigu.common.system.SystemMonitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.cdjiamigu.datasource.feign.interfaces")
 @EnableDiscoveryClient
 @EnableAutoConfiguration(exclude = MongoAutoConfiguration.class)
+@EnableScheduling
 public class RunApp {
 
     public static void main(String[] args) {
         SpringApplication.run(RunApp.class, args);
+        SystemMonitor.startMonitor("sharePlatform", 5, false);
     }
 
 }
