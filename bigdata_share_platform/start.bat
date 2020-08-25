aa70c0daca8c13b98c42172b02644b90e5953d44
PJF <1501105594@qq.com>
2020-08-25 17:18:36 +0800

    后台执行jar

--- bigdata_share_platform/start.bat
+++ bigdata_share_platform/start.bat
@@ -1,3 +1,5 @@
 title sharePlatform
 ::参考https://blog.csdn.net/qq_38974634/article/details/81710382
-java -jar sharePlatform.jar > log.file 2>&1 &
+::java -jar sharePlatform.jar > log.file 2>&1 &
+::参考https://tieba.baidu.com/p/6210448600
+nohup java -jar sharePlatform.jar &
