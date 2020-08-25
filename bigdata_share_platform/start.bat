13e92299b2f2502605974b9ab2593e67d6bff79d
PJF <1501105594@qq.com>
2020-08-25 17:38:20 +0800

    后台运行jar 并写出日志

--- bigdata_share_platform/start.bat
+++ bigdata_share_platform/start.bat
@@ -1,5 +1,9 @@
 title sharePlatform
 ::参考https://blog.csdn.net/qq_38974634/article/details/81710382
 ::java -jar sharePlatform.jar > log.file 2>&1 &
-::参考https://tieba.baidu.com/p/6210448600
-nohup java -jar sharePlatform.jar &
+::参考https://tieba.baidu.com/p/6210448600 nohup是linux下得命令
+::nohup java -jar sharePlatform.jar &
+::参考https://blog.csdn.net/tb9125256/article/details/83239571
+@echo off
+start javaw -jar sharePlatform.jar > log.file 2>&1
+exit
