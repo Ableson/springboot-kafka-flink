9409d8e8db0c871d87feb62d5710059b3446e5dc
PJF <1501105594@qq.com>
2020-08-26 15:35:35 +0800

    增加实时监控管理数据查询

--- bigdata_share_platform/src/main/java/com/dtinone/datashare/controller/MonitorController.java
+++ bigdata_share_platform/src/main/java/com/dtinone/datashare/controller/MonitorController.java
@@ -1,6 +1,9 @@
 package com.dtinone.datashare.controller;
 
+import com.cdjiamigu.common.system.SystemMonitor;
+import com.cdjiamigu.common.system.model.SM;
 import com.cdjiamigu.datasource.common.constant.ConstantCom;
+import com.dtinone.datashare.scheduled.CatalogSystemMonitor;
 import com.dtinone.datashare.util.RD;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.dtinone.datashare.scheduled.MonitorUtils.changeSmToBean;
 import static com.dtinone.datashare.scheduled.SmTask.catalogSystemMonitorTaskQueue;
 
 @RestController
@@ -49,6 +53,13 @@ public class MonitorController {
         return RD.isOk().setData(catalogSystemMonitorTaskQueue);
     }
 
+    @ApiOperation(value = "查询系统实时信息")
+    @PostMapping(value = "/getSysInfo")
+    public RD<?> getSysInfo(){
+        SM sm = SystemMonitor.getSm();
+        CatalogSystemMonitor catalogSystemMonitor = changeSmToBean(sm);
+        return RD.isOk().setData(catalogSystemMonitor);
+    }
     @ApiOperation(value = "查询当前服务状态")
     @PostMapping(value = "/getServiceStatus")
     public RD<?> getServiceStatus() {
