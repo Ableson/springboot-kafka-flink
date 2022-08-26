f9f92701d4d45d9f749c07b5daf45463b9eb947b
Ableson <67767856+Ableson@users.noreply.github.com>
2022-08-26 10:02:55 +0800

    Delete bigdata_share_platform directory

--- bigdata_share_platform/src/main/java/com/dtinone/datashare/service/ContentService.java
+++
@@ -1,9 +0,0 @@
-package com.dtinone.datashare.service;
-
-import com.dtinone.datashare.entity.InformationContent;
-import com.github.pagehelper.PageInfo;
-
-public interface ContentService {
-
-    PageInfo<InformationContent> list(InformationContent content, Integer pageNo, Integer pageSize, Integer menu);
-}
--- bigdata_share_platform/stop.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-::im 表示指定的进程名称，例如javaw.exe
-taskkill -f -t -im javaw.exe
-exit
